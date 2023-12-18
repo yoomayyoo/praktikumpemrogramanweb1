f84d5f2af4cf5eaa8b2d1b5e5c83340f824fe769
yoomayyoo <145313546+yoomayyoo@users.noreply.github.com>
2023-12-18 22:05:16 +0700

    Update PRAK604-2310817220016-NailaHanifah.c

--- PRAK604-2310817220016-NailaHanifah.c
+++ PRAK604-2310817220016-NailaHanifah.c
@@ -1,50 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
-void decodeMessage(char code[], char message[]) {
-    int len = strlen(message);
-
-    for (int i = 0; i < len; i++) {
-        if (message[i] != ' ') {
-            message[i] = (message[i] == code[i]) ? '*' : '#';
-        }
-    }
-}
-
 int main() {
-    char code[101], message[101];
-    fgets(code, sizeof(code), stdin);
-    fgets(message, sizeof(message), stdin);
-    code[strcspn(code, "\n")] = '\0';
-    message[strcspn(message, "\n")] = '\0';
+    char simbol[100], pesan[100];
+    int bintang = 0, pagar = 0;
 
-    if (strlen(code) != strlen(message)) {
+    scanf("%[^\n]%*c %[^\n]%*c", simbol, pesan);
+    
+    int len = strlen(simbol);
+
+    if (len != strlen(pesan)) {
         printf("Panjang kalimat berbeda, pesan palsu\n");
         return 0;
     }
 
-    decodeMessage(code, message);
-
-    int starCount = 0, hashCount = 0;
-    int len = strlen(message);
-
-    for (int i = 0; i < len; i++) {
-        if (message[i] == '*') {
-            starCount++;
-        } else if (message[i] == '#') {
-            hashCount++;
-        }
+    for (int i = 0; i < len; i++){
+        printf("%c", (simbol[i] == pesan[i]) ? (simbol[i] == ' ' ? ' ' : '*') : '#');
+        bintang += (simbol[i] == pesan[i]) && (simbol[i] != ' ');
+        pagar += (simbol[i] != pesan[i]);
     }
 
-    printf("%s\n", message);
-    printf("* = %d\n", starCount);
-    printf("# = %d\n", hashCount);
-
-    if (starCount >= hashCount) {
-        printf("Pesan Asli\n");
-    } else {
-        printf("Pesan Palsu\n");
-    }
+    printf("\n* = %d\n# = %d\n%s\n", bintang, pagar, bintang >= pagar ? "Pesan Asli": "Pesan Palsu");
 
     return 0;
 }
