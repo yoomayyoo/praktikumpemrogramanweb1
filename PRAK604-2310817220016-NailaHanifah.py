c734981849a69639396bd6eca835a3e780a88cd4
yoomayyoo <145313546+yoomayyoo@users.noreply.github.com>
2023-12-18 22:06:24 +0700

    Update PRAK604-2310817220016-NailaHanifah.py

--- PRAK604-2310817220016-NailaHanifah.py
+++ PRAK604-2310817220016-NailaHanifah.py
@@ -1,28 +1,20 @@
-def decode_message(code, message):
-    decoded_message = ''.join(['*' if message[i] == code[i] and message[i] != ' ' else '#' for i in range(len(message))])
-    return decoded_message
+simbol = input()
+pesan = input()
 
-def main():
-    code = input().rstrip()
-    message = input().rstrip()
+bintang = 0
+pagar = 0
 
-    if len(code) != len(message):
-        print("Panjang kalimat berbeda, pesan palsu")
-        return
+if len(simbol) != len(pesan):
+    print("Panjang kalimat berbeda, pesan palsu")
+else:
+    for i in range(len(simbol)):
+        if simbol[i] == pesan[i]:
+            print('*', end='' if simbol[i] == ' ' else ' ')
+            bintang += 1 if simbol[i] != ' ' else 0
+        else:
+            print('#', end='')
+            pagar += 1
 
-    decoded_message = decode_message(code, message)
-    
-    star_count = decoded_message.count('*')
-    hash_count = decoded_message.count('#')
-
-    print(decoded_message)
-    print(f"* = {star_count}")
-    print(f"# = {hash_count}")
-
-    if star_count >= hash_count:
-        print("Pesan Asli")
-    else:
-        print("Pesan Palsu")
-
-if __name__ == "__main__":
-    main()
+    print("\n* =", bintang)
+    print("# =", pagar)
+    print("Pesan Asli" if bintang >= pagar else "Pesan Palsu")
